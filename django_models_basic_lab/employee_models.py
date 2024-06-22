47ddbb1dffd70504ec2ec72977fa9112dc34c25e
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-06-22 11:59:11 +0300

    Update employee_models.py

--- django_models_basic_lab/employee_models.py
+++ django_models_basic_lab/employee_models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class User(models.Model):
     name = models.CharField(max_length=30)
     email_address = models.EmailField()
@@ -9,3 +10,37 @@ class User(models.Model):
     birth_date = models.DateField()
     works_full_time = models.BooleanField()
     created_in = models.DateTimeField(auto_now_add=True)
+
+
+class Department(models.Model):
+
+    class LocationChoices(models.TextChoices):
+        VARNA = 'Varna', 'Varna'
+        BURGAS = 'Burgas', 'Burgas'
+        PLOVDIV = 'Plovidv', 'Plovdiv'
+        SOFIA = 'Sofia', 'Sofia'
+
+    code = models.CharField(
+        max_length=4,
+        primary_key=True,
+        unique=True
+    )
+    name = models.CharField(
+        max_length=50,
+        unique=True
+    )
+    employees_count = models.PositiveIntegerField(
+        default=1,
+        verbose_name= 'Employees Count'
+    )
+
+    location = models.CharField(
+        max_length=20,
+        choices=LocationChoices,
+        Null=True
+    )
+    last_edited_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+    
