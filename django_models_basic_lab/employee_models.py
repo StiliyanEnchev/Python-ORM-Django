bffa531f0cfed302871c4f4cef44badf20601e09
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-06-23 15:06:19 +0300

    Update employee_models.py

--- django_models_basic_lab/employee_models.py
+++ django_models_basic_lab/employee_models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 # Creat e your models here.
@@ -42,4 +44,52 @@ class Department(models.Model):
     last_edited_on = models.DateTimeField(
         auto_now=True,
         editable=False
+    )
+
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    budget = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+
+    duration_in_days = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        verbose_name="Duration in Days"
+    )
+
+    estimated_hours = models.FloatField(
+        blank=True,
+        null=True,
+        verbose_name="Estimated Hours"
+    )
+
+    start_date = models.DateField(
+        verbose_name='Start Date',
+        blank=True,
+        null=True,
+        default=date.today()
+    )
+
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+
+    last_edited_on = models.DateTimeField(
+        editable=False,
+        auto_now=True
     )
